9e89a431eee4d4cc947faa27f5fa4712f36851ef
Balaji Kuppusamy <balaji.kuppusamy@aditro.com>
2017-06-08 18:31:09 +0200

    Configure application settings via Docker compose

---
+++ PracticeCoreWebApp/ApplicationSettings.cs
@@ -0,0 +1,15 @@
+﻿namespace PracticeCoreWebApp
+{
+    public class ApplicationSettings
+    {
+        public string ClaimTypeEmail { get; set; }
+
+        public string BaseUrlHcm { get; set; }
+
+        public string HateoasAcceptHeader { get; set; }
+
+        public string GraphUrl { get; set; }
+
+        public string PeopleServiceUrl { get; set; }
+    }
+}
--- PracticeCoreWebApp/Controllers/ValuesController.cs
+++ PracticeCoreWebApp/Controllers/ValuesController.cs
@@ -1,19 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace PracticeCoreWebApp.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        private readonly ApplicationSettings appSettings;
+        private readonly IHostingEnvironment hosting;
+
+        public ValuesController(IOptions<ApplicationSettings> appSettings, IHostingEnvironment hosting)
+        {
+            this.hosting = hosting;
+            this.appSettings = appSettings.Value;
+        }
+
         // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new[] { appSettings.GraphUrl, appSettings.PeopleServiceUrl, hosting.EnvironmentName };
         }
 
         // GET api/values/5
